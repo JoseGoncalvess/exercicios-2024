e544e4c7e1b09a46617b15c27b61f3fbe1e5932a
José Gonçalves <joseinacio774@gmail.com>
2024-04-27 11:34:12 -0300

    [Estrutura] Ajust idetation

--- lib/pages/person details/person_details.dart
+++ lib/pages/person details/person_details.dart
@@ -1,32 +1,26 @@
 import 'package:chuva_dart/pages/person%20details/widgets/custom_details_person.dart';
 import 'package:flutter/material.dart';
 
-import '../../widgets/custom_app_bar_widget.dart';
+import '../../components/custom_app_bar_widget.dart';
 
 class PersonDetails extends StatefulWidget {
-
-  const PersonDetails({ super.key });
+  const PersonDetails({super.key});
 
   @override
   State<PersonDetails> createState() => _PersonDetailsState();
 }
 
 class _PersonDetailsState extends State<PersonDetails> {
-
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-         body: Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: Column(
-            children: [
-              CustomAppBarWidget(),
-              CustomDetailsPerson ()
-              
-            ],
-          ),
-         ),
-       );
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          children: [CustomAppBarWidget(), CustomDetailsPerson()],
+        ),
+      ),
+    );
   }
 }
