7598f730ce22f45322f2e534698e7cffef8bc7e4
JoseGoncalvess <joseinacio774@gmail.com>
2024-04-29 14:04:43 -0300

    [logic] read from json data

--- lib/services/repository/repository.dart
+++ lib/services/repository/repository.dart
@@ -3,10 +3,30 @@ import 'package:chuva_dart/services/repository/models/data_models.dart';
 import 'package:flutter/services.dart';
 
 class Repository {
-  Future<DataModels> readJson() async {
+  Future<List<DataModels>> readJson() async {
     final String response =
         await rootBundle.loadString('assets/activities.json');
-    final data = await json.decode(response);
-    return DataModels.fromJson(data);
+    final String response2 =
+        await rootBundle.loadString('assets/activities-1.json');
+
+    return [ DataModels.fromJson(json.decode(response)),DataModels.fromJson(json.decode(response2))];
   }
+
+  Future<DataModels> loaddata() async{
+    List<DataModels> listaData = await readJson();
+    DataModels dataBase = listaData[0];
+    List<int> idnumber =[];
+
+      for (var ev in dataBase.data) {
+        idnumber.add(ev.id);
+      }
+      for (var e in listaData[1].data) {
+        if (!idnumber.contains(e.id)) {
+          dataBase.data.add(e);
+        }
+      }
+      dataBase.count = dataBase.data.length;
+      return dataBase;
+  }
+  
 }
